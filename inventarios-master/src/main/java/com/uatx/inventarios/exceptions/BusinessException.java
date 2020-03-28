657f66a7b9e6176b29863ae5a38d30b2595ae8b5
enrique199 <enriquesander7@gmail.com>
2020-03-28 10:15:33 -0600

    Revert "Inventario"
    This reverts commit c8f672da17aee6da5de4776e779722d2d993e4c9.

--- inventarios-master/src/main/java/com/uatx/inventarios/exceptions/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package com.uatx.inventarios.exceptions;
-
-public class BusinessException extends RuntimeException {
-
-    public BusinessException(String mensaje){
-        super(mensaje);
-    }
-}
